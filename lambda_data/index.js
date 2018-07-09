9ebc45063bf2e25ea446393919918d1ee36eaa15
Sunny Miglani <sm15504@my.bristol.ac.uk>
2018-07-09 10:31:02 +0100

    added the text related to test, need to see how to save session attributes!

--- lambda_data/index.js
+++ lambda_data/index.js
@@ -1,30 +1,35 @@
 "use strict";
 const Alexa = require('alexa-sdk');
-const ssmlMediumBreak = "<break time = '0.3s' />";
+const ssmlMediumBreak = "<break time = '0.3s'/>";
 
 var firstTime = true;
 
-var verboseIntentSpeech = "The German people reacted to the outbreak of war with resignation <break time = '0.3s' />  <break time = '0.3s' /> \
-In 1914 <break time = '0.3s' />  at the outbreak of World War One <break time = '0.3s' />  \
-there had been much enthusiasm but not this time <break time = '0.3s' />  <break time = '0.3s' /> \
-Germans in 1939 could still remember the pain of 1918 <break time = '0.3s' />  their surrender and the subsequent punishment at Versailles\
- <break time = '0.3s' />  <break time = '0.3s' /> However <break time = '0.3s' />  the vast majority of \
- Germans reluctantly supported the war and signed up to play their part in the war\
-  effort <break time = '0.3s' />  <break time = '0.3s' /> \
+var startValue = 0;
+
+var verboseIntentSpeech =  "There were approximately 45 million Protestants and 22 million Catholic Christians in Germany in 1933 <break time = '0.3s' /> Hitler saw Christianity as a threat and a potential source of opposition to Nazism because it emphasised peace <break time = '0.3s' /> The Nazis tried to control the Churches with policies and bargaining <break time = '0.3s' /> \
+A state Reich Church under the leadership of the Nazi Bishop Ludwig Müller was established to unify the different branches of Protestantism <break time = '0.3s' /> This enabled the Nazis to use a group called the ‘German Christians’ within the Reich Church to promote Nazi ideas <break time = '0.3s' /> \
+In 1933 Hitler agreed a Concordat with the Pope <break time = '0.15s' /> which said that he would not interfere in the running of the Catholic Church if it stayed out of political matters <break time = '0.3s' /> Hitler didn’t keep his side of the bargain <break time = '0.15s' /> however <break time = '0.15s' /> as the Nazis attempted to infiltrate the Church and spread their propaganda <break time = '0.3s' /> \
+The Reich Church attempted to ban the use of the Old Testament in religious services as it was considered a ‘Jewish book’ <break time='0.3s' />  Eight hundred Pastors of the Confessional Church <break time = '0.15s' /> a non - conforming Protestant group <break time = '0.15s' /> were arrested and sent to concentration camps <break time = '0.3s' /> \
+The Nazis attempted to stop Catholics using the crucifix in church <break time = '0.15s' /> though this attempt was not successful <break time = '0.3s' /> Catholic schools and youth organisations were supressed <break time = '0.15s' />  with German children being educated in state schools and taught a Nazi curriculum <break time = '0.15s' /> as well as being expected to join the various branches of the Hitler Youth <break time = '0.3s' /> Catholic newspapers were banned and four hundred Catholic priests were sent to Dachau concentration camp <break time = '0.3s' /> \
+In 1937 Hitler was forced to return control of the Church to the old Protestant leadership <break time = '0.15s' />  in return for a promise that the Church would stay out of politics <break time = '0.3s' /> \
+Attendance at Catholic churches increased substantially under the Nazis <break time = '0.15s' /> especially during World War Two <break time = '0.15s' /> showing that Hitler’s attempts to reduce the influence of religion in Germany was ultimately unsuccessful <break time = '0.3s' /> \
 "
 
-var ShortIntentA = "The German people reacted to the outbreak of war with resignation <break time = '0.3s' />  <break time = '0.3s' /> \
-In 1914 <break time = '0.3s' />  at the outbreak of World War One <break time = '0.3s' />  \
-there had been much enthusiasm but not this time";
+const shortIntentSpeechA = "There were approximately 45 million Protestants and 22 million Catholic Christians in Germany in 1933 <break time = '0.3s' /> Hitler saw Christianity as a threat and a potential source of opposition to Nazism because it emphasised peace <break time = '0.3s' /> The Nazis tried to control the Churches with policies and bargaining <break time = '0.3s' /> \
+A state Reich Church under the leadership of the Nazi Bishop Ludwig Müller was established to unify the different branches of Protestantism <break time = '0.3s' /> This enabled the Nazis to use a group called the ‘German Christians’ within the Reich Church to promote Nazi ideas <break time = '0.3s' /> \
+In 1933 Hitler agreed a Concordat with the Pope <break time = '0.15s' /> which said that he would not interfere in the running of the Catholic Church if it stayed out of political matters <break time = '0.3s' /> "
 
-var ShortIntentB = "Germans in 1939 could still remember the pain of 1918 <break time = '0.3s' />  their surrender and the subsequent punishment at Versailles\
- <break time = '0.3s' />  <break time = '0.3s' /> However <break time = '0.3s' />  the vast majority of \
- Germans reluctantly supported the war and signed up to play their part in the war\
-  effort";
+const shortIntentSpeechB = "Hitler didn’t keep his side of the bargain <break time = '0.15s' /> however <break time = '0.15s' /> as the Nazis attempted to infiltrate the Church and spread their propaganda <break time = '0.3s' /> \
+The Reich Church attempted to ban the use of the Old Testament in religious services as it was considered a ‘Jewish book’ <break time='0.3s' />  Eight hundred Pastors of the Confessional Church <break time = '0.15s' /> a non - conforming Protestant group <break time = '0.15s' /> were arrested and sent to concentration camps <break time = '0.3s' /> \
+The Nazis attempted to stop Catholics using the crucifix in church <break time = '0.15s' /> though this attempt was not successful <break time = '0.3s' /> Catholic schools and youth organisations were supressed <break time = '0.15s' />  with German children being educated in state schools and taught a Nazi curriculum <break time = '0.15s' /> as well as being expected to join the various branches of the Hitler Youth <break time = '0.3s' /> "
 
-var shortIntentFirstSpeech = "I am going to break down the paragraph and < break time = '0.3s' /> only continue once you're ready for the paragraph about Germany" + ssmlMediumBreak  + ShortIntentA + ssmlMediumBreak;
+const shortIntentSpeechC = "Catholic newspapers were banned and four hundred Catholic priests were sent to Dachau concentration camp <break time='0.3s' /> \
+In 1937 Hitler was forced to return control of the Church to the old Protestant leadership <break time = '0.15s' />  in return for a promise that the Church would stay out of politics <break time = '0.3s' /> \
+Attendance at Catholic churches increased substantially under the Nazis <break time = '0.15s' /> especially during World War Two <break time = '0.15s' /> showing that Hitler’s attempts to reduce the influence of religion in Germany was ultimately unsuccessful <break time = '0.3s' /> \
+"
 
-var shortIntentSecondSpeech = ShortIntentB;
+var shortIntentFirstSpeech = "I am going to break down the paragraph and <break time = '0.3s' /> only continue once you're ready for the paragraph about Germany" + ssmlMediumBreak  + shortIntentSpeechA + ssmlMediumBreak;
+
 
 var handlers = {
 
@@ -35,7 +40,7 @@ var handlers = {
   },
 
   'HelloIntent': function () {
-    this.response.speak("Hello! Welcome to big historian <break time = '0.3s' />  <break time = '0.3s' />  Ask me to give you a large or small explanation <break time = '0.3s' />  <break time = '0.3s' />  This should help me learn what's the best way to communicate!").listen();
+    this.response.speak("Hello! Welcome to big historian <break time = '0.3s' />  <break time = '0.3s' />  Ask me to give you a large or small explanation <break time = '0.3s' />  <break time = '0.3s' />  This should help me learn what's the best way to communicate!");
     this.emit(':responseReady');
   },
 
@@ -51,6 +56,7 @@ var handlers = {
   },
 
   'VerboseIntent': function(){
+    
     var verboseIntentSpeechLocal = "I am going to tell you about Germany and the World War 2" + ssmlMediumBreak + "Here I go!" + ssmlMediumBreak + verboseIntentSpeech;
     this.response.speak(verboseIntentSpeechLocal);
     this.emit(':responseReady');
@@ -58,19 +64,90 @@ var handlers = {
 
   'ShortIntent': function(){
 
-    var ShortIntentA = "The German people reacted to the outbreak of world war 2 with resignation <break time = '0.3s' /> \
-    In 1914 <break time = '0.3s' />  at the outbreak of World War One \
-    there had been much enthusiasm but not this time";
+    console.log(this.attributes.isInit);
+    console.log(this.attributes.index);
+    
+    if(this.attributes.isInit == null || this.attributes.isInit === false){
+      this.attributes.isInit = true;
+      this.attributes.index = 0;
+    }
+    else if(this.attributes.isInit === true){
+      this.attributes.index +=1;
+      
+      if(this.attributes.index > 4){
+        this.attributes.index = 0;
+      }
+    }
+    
+    const shortIntentSpeechA = "There were approximately 45 million Protestants and 22 million Catholic Christians in Germany in 1933 <break time = '0.3s' /> Hitler saw Christianity as a threat and a potential source of opposition to Nazism because it emphasised peace <break time = '0.3s' /> The Nazis tried to control the Churches with policies and bargaining <break time = '0.3s' /> \
+    A state Reich Church under the leadership of the Nazi Bishop Ludwig Müller was established to unify the different branches of Protestantism <break time = '0.3s' /> "
+    
+    const shortIntentSpeechB = "This enabled the Nazis to use a group called the ‘German Christians’ within the Reich Church to promote Nazi ideas <break time='0.3s' /> \
+    In 1933 Hitler agreed a Concordat with the Pope <break time = '0.15s' /> which said that he would not interfere in the running of the Catholic Church if it stayed out of political matters <break time = '0.3s' /> "
+    
+    const shortIntentSpeechC = "Hitler didn’t keep his side of the bargain <break time = '0.15s' /> however <break time = '0.15s' /> as the Nazis attempted to infiltrate the Church and spread their propaganda <break time = '0.3s' /> \
+    The Reich Church attempted to ban the use of the Old Testament in religious services as it was considered a ‘Jewish book’ <break time='0.3s' /> "
+    
+    const shortIntentSpeechD = "Eight hundred Pastors of the Confessional Church <break time = '0.15s' /> a non - conforming Protestant group <break time = '0.15s' /> were arrested and sent to concentration camps <break time = '0.3s' /> \
+    The Nazis attempted to stop Catholics using the crucifix in church <break time = '0.15s' /> though this attempt was not successful <break time = '0.3s' /> Catholic schools and youth organisations were supressed <break time = '0.15s' />  with German children being educated in state schools and taught a Nazi curriculum <break time = '0.15s' /> as well as being expected to join the various branches of the Hitler Youth <break time = '0.3s' /> "
+    
+    
+    var speakingString = "Problem occured!";
+    console.log(this.attributes.index);
+    
+    if(this.attributes.index === 0){
+      
+      var shortIntentFirstSpeech = "I am going to break down the paragraph <break time = '0.15s' /> and only continue once you're ready <break time = '0.3s' /> Here I go!" + ssmlMediumBreak + shortIntentSpeechA + ssmlMediumBreak + "If you would like to hear more, say Alexa, ask\
+      big historian for the next part";
+      speakingString = shortIntentSpeechA;
+
+      this.attributes.index +=1;
+
+      this.response.shouldEndSession = false;
+            
+      console.log(this.attributes.index);
+      this.response.speak(shortIntentFirstSpeech);
+      this.emit(":responseReady");
+      return;
+    }
+    else if(this.attributes.index == 1){
+      this.attributes.index += 1;
+      speakingString = shortIntentSpeechA;
+    }
+    else if(this.attributes.index == 2){
+      this.attributes.index += 1;
+      speakingString = shortIntentSpeechB;
+    }
+    else if (this.attributes.index == 3) {
+      this.attributes.index += 1;
+      speakingString = shortIntentSpeechC;
+    }
+
+    else if (this.attributes.index == 4) {
+      this.attributes.index += 1;
+      speakingString = shortIntentSpeechD;
+    }
+
+    var shortIntentSpeech = ssmlMediumBreak + speakingString + ssmlMediumBreak + "If you would like to hear more , say Alexa, ask big historian for the next part";
 
 
-    var shortIntentFirstSpeech = "I am going to break down a paragraph about Germany "+  ssmlMediumBreak +" and shall only continue with the second part of the paragraph when you're ready" + ssmlMediumBreak + ShortIntentA + ssmlMediumBreak;
-
-
-
-    this.response.speak(shortIntentFirstSpeech);
+    this.response.shouldEndSession = false;
+    this.response.speak(shortIntentSpeech);
     this.emit(':responseReady');
 
   },
+
+  'ResetValuesIntent' : function(){
+    this.attributes.index = 0;
+    this.attributes.isInit = false;
+    this.response.speak("Resetting the values!");
+    this.emit(":responseReady");
+  },
+
+  'NextIntent': function(){
+    this.emit("ShortIntent");
+  }
+
 };
 
 
--- replaceComma.py
+++ replaceComma.py
@@ -5,6 +5,8 @@ ssmlSmallBreak = " <break time = '0.15s' /> ";
 def makeAlexaFriendly(inputText):
     myText = "";
     for char in inputText:
+        if(char == '\n'):
+            continue;
         appendChar = char;
         if(char == "," or char == ";"):
             appendChar = ssmlSmallBreak;
@@ -17,10 +19,10 @@ def flow():
     print("Please enter the string you would like converted into Alexa friendly speech");
     for line in sys.stdin:
         if(line != ""):
-            friendly = makeAlexaFriendly(line);    
-            print("The alexa friendly text is : ");
+            friendly = makeAlexaFriendly(line);
+            print("\n-----------------------\n");
             print(friendly);
-            print("Restart the program if you'd like to convert again!");
+            print("\n-----------------------\n")
         else:
             print("Check text please!");
 
